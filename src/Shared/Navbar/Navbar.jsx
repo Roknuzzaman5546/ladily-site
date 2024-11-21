a23c84ac6e801c426e43bc086b9e0c3dd3d06f90
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-11-21 21:45:09 +0600

    down navbar code added

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -28,10 +28,10 @@ const Navbar = () => {
             <div className={`md:block hidden relative shadow-md`}>
                 {/* Upper menu */}
                 <div className="border-b-[1.5px] border-[#d4d4d4]">
-                    <div className={`flex justify-between items-center h-[63px] sm:w-[540px] md:w-[720px] lg:w-[966px] xl:w-[1140px] 2xl:w-[1360px] mx-auto pr-0 md:pr-[6px] lg:pr-[6px] xl:pr-0`}>
+                    <div className={`flex justify-between items-center xl:h-[63px] lg:h-[62.5px] sm:w-[540px] md:w-[720px] lg:w-[966px] xl:w-[1140px] 2xl:w-[1360px] mx-auto pr-0 md:pr-[6px] lg:pr-[6px] xl:pr-0`}>
                         <div className='flex items-center'>
                             <a href="">
-                                <img className=' w-[100px]' src={logo} alt="" />
+                                <img className=' xl:w-[100px] lg:w-[90px]' src={logo} alt="" />
                             </a>
                         </div>
                         <div className="flex items-end -mt-[2px]">
@@ -40,7 +40,7 @@ const Navbar = () => {
                                     <input
                                         type="text"
                                         placeholder="Search in Ladily"
-                                        className="placeholder:font-cormorant placeholder:font-semibold border-none outline-0 ring-0 w-full pb-[1px]"
+                                        className="placeholder:font-cormorant placeholder:font-semibold border-none outline-0 ring-0 w-full xl:pb-[1px] lg:pb-[3px]"
                                     />
                                     <button>
                                         <SearchIcon />
@@ -68,14 +68,14 @@ const Navbar = () => {
                                         <h2 className="text-3xl font-cormorant font-bold">Your Cart</h2>
                                         <p className="text-3xl font-bold">(0)</p>
                                     </div>
-                                    <h3 className=" font-cormorant text-xl pt-16">No items in your cart</h3>
+                                    <h3 className=" font-cormorant text-lg pt-16">No items in your cart</h3>
                                 </div>
                             </button>
                         </div>
                     </div>
                 </div>
                 {/* down menu */}
-                <div className="w-full bg-white flex justify-center items-center h-[55px]">
+                <div className="w-full bg-white flex justify-center items-center gap-12 h-[61px]">
                     <div>
                         <nav className=" flex justify-center items-center gap-2">
                             <li className=" font-cormorant text-xl font-semibold list-none">Face</li>
@@ -84,19 +84,19 @@ const Navbar = () => {
                     </div>
                     <div>
                         <nav className=" flex justify-center items-center gap-2">
-                            <li className=" font-cormorant text-xl font-semibold list-none">Eyes</li>
+                            <li className=" font-cormorant text-lg font-semibold list-none">Eyes</li>
                             <p><ChevronDownIcon /></p>
                         </nav>
                     </div>
                     <div>
                         <nav className=" flex justify-center items-center gap-2">
-                            <li className=" font-cormorant text-xl font-semibold list-none">Hair</li>
+                            <li className=" font-cormorant text-lg font-semibold list-none">Hair</li>
                             <p><ChevronDownIcon /></p>
                         </nav>
                     </div>
                     <div>
                         <nav className=" flex justify-center items-center gap-2">
-                            <li className=" font-cormorant text-xl font-semibold list-none">Body care</li>
+                            <li className=" font-cormorant text-lg font-semibold list-none">Body care</li>
                             <p><ChevronDownIcon /></p>
                         </nav>
                     </div>
